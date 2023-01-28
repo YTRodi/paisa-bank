92666866a0410c838372d327977b4d0375670b6c
YTRodi <yagorodi@gmail.com>
2023-01-28 18:12:24 -0300

    feat: STRINGS resource

---
+++ src/resources/strings.ts
@@ -0,0 +1,27 @@
+import { BRAND_NAME } from '~/constants'
+
+export const STRINGS = {
+  LOGIN: {
+    BRAND_NAME,
+    BRAND_TEXT: 'Comienza a manejar tu vida financiera',
+    EMAIL_INPUT_LABEL: 'Email',
+    EMAIL_INPUT_PLACEHOLDER: 'Ingresa  tu email',
+    /**
+      EMAIL_INPUT_ERRORS: {
+        INVALID: 'El email es inválido',
+        ALREADY_IN_USE: 'El email ya está en uso'
+      }
+    */
+    PASSWORD_INPUT_LABEL: 'Contraseña',
+    PASSWORD_INPUT_PLACEHOLDER: 'Ingresa tu contraseña',
+    /**
+      PASSWORD_INPUT_ERRORS: {
+        INVALID: 'La contraseña es inválida',
+      }
+    */
+    REMIND_ME: 'Recordarme',
+    DONT_HAVE_AN_ACCOUNT: 'No tienes cuenta?',
+    REGISTER_HERE: 'Regístrate',
+    LOGIN_BUTTON: 'Ingresar',
+  },
+}
