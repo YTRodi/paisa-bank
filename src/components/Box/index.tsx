0108bfd4d3b928a14e0b6951aa5c3e36119c609e
YTRodi <yagorodi@gmail.com>
2023-01-28 18:13:53 -0300

    feat: export Box and Text props

--- src/components/Box/index.tsx
+++ src/components/Box/index.tsx
@@ -1,5 +1,6 @@
-import { createBox } from '@shopify/restyle'
+import { createBox, type BoxProps as RestyleBoxProps } from '@shopify/restyle'
 
 import { type Theme } from '~/styles/theme'
 
 export const Box = createBox<Theme>()
+export type BoxProps = RestyleBoxProps<Theme>
