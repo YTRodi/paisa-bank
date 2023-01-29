e0b158ac14a8ff0a3a81dda0fcf03266cea35eb6
YTRodi <yagorodi@gmail.com>
2023-01-29 10:59:21 -0300

    feat: WIP Login screen

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,4 +1,5 @@
-import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { type Control, useForm, type FormState } from 'react-hook-form'
 import { SafeAreaView } from 'react-native'
 
 import {
@@ -12,6 +13,12 @@ import {
 } from '~/components'
 import { useTheme } from '~/hooks'
 import { STRINGS } from '~/resources'
+import { LOGIN_SCHEMA } from '~/schemas'
+
+export interface LoginFormValues {
+  email: string
+  password: string
+}
 
 const {
   BRAND_NAME,
@@ -28,29 +35,43 @@ const {
 
 export const Login = () => {
   const theme = useTheme()
-  // TODO: me quedé acá integrando zod + rhf
-  // TODO: me quedé acá integrando zod + rhf
-  // TODO: me quedé acá integrando zod + rhf
-  // TODO: me quedé acá integrando zod + rhf
-  // TODO: me quedé acá integrando zod + rhf
-  // TODO: me quedé acá integrando zod + rhf
-  // TODO: me quedé acá integrando zod + rhf
-  const { control } = useForm()
 
   return (
     <SafeAreaView
       // style={{ flex: 1, backgroundColor: theme.colors.$mainBackground }}
       style={{ flex: 1, backgroundColor: theme.colors.lightblueLight }}
     >
-      {/* TODO: create <Form /> */}
-      <Box flex={1} paddingHorizontal="md">
-        <Box flex={1} justifyContent="center">
-          <LoginHeader />
-          <LoginFormFields control={control} />
-        </Box>
-        <LoginFooter />
-      </Box>
+      <LoginForm />
     </SafeAreaView>
+  )
+}
+
+const LoginForm = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: { isValid, errors },
+  } = useForm<LoginFormValues>({
+    resolver: yupResolver(LOGIN_SCHEMA),
+  })
+
+  const onSubmit = (data: LoginFormValues) => {
+    if (!isValid) {
+      return
+    }
+
+    // eslint-disable-next-line no-console
+    console.log(data)
+  }
+
+  return (
+    <Box flex={1} paddingHorizontal="md">
+      <Box flex={1} justifyContent="center">
+        <LoginHeader />
+        <LoginFormFields control={control} errors={errors} />
+      </Box>
+      <LoginFormFooter onSubmit={handleSubmit(onSubmit)} />
+    </Box>
   )
 }
 
@@ -66,20 +87,30 @@ const LoginHeader = () => {
   )
 }
 
-// eslint-disable-next-line react/prop-types
-const LoginFormFields = ({ control }) => {
+const LoginFormFields = ({
+  errors,
+  control,
+}: {
+  errors: FormState<LoginFormValues>['errors']
+  control: Control<LoginFormValues, any>
+}) => {
   return (
     <Box>
       <FormElement label={EMAIL_INPUT_LABEL} marginBottom="xl">
         <TextInput
           control={control}
+          error={errors.email?.message}
+          hasError={Boolean(errors.email)}
           name="email"
           placeholder={EMAIL_INPUT_PLACEHOLDER}
         />
       </FormElement>
       <FormElement label={PASSWORD_INPUT_LABEL} marginBottom="md">
         <TextInput
+          secureTextEntry
           control={control}
+          error={errors.password?.message}
+          hasError={Boolean(errors.password)}
           name="password"
           placeholder={PASSWORD_INPUT_PLACEHOLDER}
         />
@@ -95,11 +126,11 @@ const LoginFormFields = ({ control }) => {
   )
 }
 
-const LoginFooter = () => {
+const LoginFormFooter = ({ onSubmit }: { onSubmit: () => void }) => {
   return (
     <Box>
       <LoginSignUpSection marginBottom="2xl" />
-      <Button label={LOGIN_BUTTON} onPress={() => {}} />
+      <Button label={LOGIN_BUTTON} onPress={onSubmit} />
     </Box>
   )
 }
@@ -113,9 +144,3 @@ const LoginSignUpSection = ({ ...props }: BoxProps) => {
     </Box>
   )
 }
-
-/**
- * - RHF
- * - Zod
- * - RHF SmartComponents
- */
