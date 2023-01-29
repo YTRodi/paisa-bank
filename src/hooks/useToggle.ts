78f3def40d3d2adcd92e3b27578913b83f242447
YTRodi <yagorodi@gmail.com>
2023-01-29 10:58:12 -0300

    feat: useToggle custom hook

---
+++ src/hooks/useToggle.ts
@@ -0,0 +1,13 @@
+import { useCallback, useState } from 'react'
+
+function useToggle(initialState = false): [boolean, () => void] {
+  const [state, setState] = useState(initialState)
+
+  const toggle = useCallback(() => {
+    setState((state) => !state)
+  }, [])
+
+  return [state, toggle]
+}
+
+export { useToggle }
