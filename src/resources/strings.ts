fa227851a19487207c39ac1540e147d9d9bc728e
YTRodi <yagorodi@gmail.com>
2023-01-29 10:58:29 -0300

    feat: PASSWORD_INPUT strings

--- src/resources/strings.ts
+++ src/resources/strings.ts
@@ -6,19 +6,16 @@ export const STRINGS = {
     BRAND_TEXT: 'Comienza a manejar tu vida financiera',
     EMAIL_INPUT_LABEL: 'Email',
     EMAIL_INPUT_PLACEHOLDER: 'Ingresa  tu email',
-    /**
-      EMAIL_INPUT_ERRORS: {
-        INVALID: 'El email es inválido',
-        ALREADY_IN_USE: 'El email ya está en uso'
-      }
-    */
+    EMAIL_INPUT_ERRORS: {
+      REQUIRED: 'El email es requerido',
+      INVALID: 'El email es inválido',
+    },
     PASSWORD_INPUT_LABEL: 'Contraseña',
     PASSWORD_INPUT_PLACEHOLDER: 'Ingresa tu contraseña',
-    /**
-      PASSWORD_INPUT_ERRORS: {
-        INVALID: 'La contraseña es inválida',
-      }
-    */
+    PASSWORD_INPUT_ERRORS: {
+      REQUIRED: 'La contraseña es requerida',
+      // INVALID: 'La contraseña es inválida', // TODO: add regex pattern
+    },
     REMIND_ME: 'Recordarme',
     DONT_HAVE_AN_ACCOUNT: 'No tienes cuenta?',
     REGISTER_HERE: 'Regístrate',
