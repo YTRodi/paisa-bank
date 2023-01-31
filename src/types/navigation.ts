85711d9a0b0f161dc56a83e883d670e12c1486dd
YTRodi <yagorodi@gmail.com>
2023-01-31 20:56:41 -0300

    feat: navigation types

---
+++ src/types/navigation.ts
@@ -0,0 +1,29 @@
+/* eslint-disable @typescript-eslint/consistent-type-definitions */
+import { type BottomTabScreenProps } from '@react-navigation/bottom-tabs'
+import { type CompositeScreenProps } from '@react-navigation/native'
+import { type NativeStackScreenProps } from '@react-navigation/native-stack'
+
+export type RootStackParamList = {
+  Login: undefined
+  AuthenticatedBottomTabNavigator: undefined
+}
+
+export type LoginScreenProps = NativeStackScreenProps<
+  RootStackParamList,
+  'Login'
+>
+
+export type AuthenticatedBottomTabParamList = {
+  Home: undefined
+  Contacts: undefined
+}
+
+export type HomeScreenProps = CompositeScreenProps<
+  BottomTabScreenProps<AuthenticatedBottomTabParamList, 'Home'>,
+  NativeStackScreenProps<RootStackParamList>
+>
+
+export type ContactsScreenProps = CompositeScreenProps<
+  BottomTabScreenProps<AuthenticatedBottomTabParamList, 'Contacts'>,
+  NativeStackScreenProps<RootStackParamList>
+>
